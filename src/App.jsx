b27ef2ea36cbeb99fc08f26f119827fc286514c0
Nicolas <hernandeznicolas068@gmail.com>
2023-12-11 16:48:28 -0300

    nuevo committttt

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   }, []);
 
   return (
-    <div className="bg-white">
+    <div className="bg-black">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
