efd8a67c1a0e3913f6d100568e7e858dd5fb42ec
Nicolas <hernandeznicolas068@gmail.com>
2023-12-13 14:56:02 -0300

    miercoles

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,14 @@ function App() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setPosition((prevPosition) => (prevPosition + 1) % 100);
-    }, 50);
+      setPosition((prevPosition) => (prevPosition + 0.1) % 101);
+    }, 20);
 
     return () => clearInterval(interval);
   }, []);
 
+  const levitationOffset = 10;
+  const levitationAmount = Math.sin((position / 50) * Math.PI) * levitationOffset;
   return (
     <div className="bg-black">
       <header className="absolute inset-x-0 top-0 z-50">
@@ -111,6 +113,10 @@ function App() {
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 transition-opacity duration-500 ease-in-out"
           aria-hidden="true"
+          style={{
+            transform: `translateY(${levitationAmount}px)`,
+            opacity: position === 0 ? 0 : 1,  // Hacer que aparezca suavemente al reiniciar
+          }}
         >
           <div
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#8090ff] to-[#0069ff] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem] animated-fadeIn"
@@ -118,23 +124,25 @@ function App() {
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
               transform: `translateX(${position}%)`,
+              animation: 'fadeIn 1s',
             }}
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-white ring-1 ring-gray-900/10 hover:ring-gray-100">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-indigo-600">
+              Bienvenido a mi sitio web, Si te interesa mi trabajo: {' '}
+              <a href="#" className="font-semibold text-cyan-400">
                 <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
+                Contáctame <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
           </div>
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-              Data to enrich your online business
-            </h1>
+          <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+          <span className="bg-gradient-to-br from-cyan-200 to-cyan-500 text-transparent bg-clip-text">Analista </span>
+            Programador  Desarrollador   <span className="bg-gradient-to-br from-sky-500 to-sky-500 text-transparent bg-clip-text"> Front End</span>
+          </h1>
             <p className="mt-6 text-lg leading-8 text-white">
             Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua
             </p>
@@ -142,9 +150,7 @@ function App() {
               <a className=" cursor-pointer text-white border border-sky-700 bg-gradient-to-br from-sky-700/10 hover:from-sky-500/40 transition to-black-500/20 hover:to-black-500/50 px-8 py-4 rounded-md">
               Ver Proyectos</a>
             
-              <a href="#" className="text-sm font-semibold leading-6 text-white">
-                Abrir CV <span aria-hidden="true">→</span>
-              </a>
+              <a href="/" class="border border-zinc-400 bg-zinc-200 hover:bg-zinc-800/80 hover:text-white transition bg-zinc-900/50 text-zinc-300 px-8 py-4 rounded-md">Abrir CV </a>
             </div>
           </div>
         </div>
@@ -153,10 +159,12 @@ function App() {
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#8090ff] to-[#0069ff] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            className="relative right-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#8090ff] to-[#0069ff] opacity-30 sm:right-[calc(0%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                 transform: `translateX(${position}%)`,
+                 
             }}
           />
         </div>
